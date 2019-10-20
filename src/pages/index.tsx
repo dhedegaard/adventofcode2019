857763a46b0ac73cde4515b16f5809f61920c59b
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-10-20 21:39:23 +0200

    index: Made proper H1.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react"
 
 import Layout from "../components/Layout"
-import { Typography } from "@material-ui/core"
+import { Typography, Paper } from "@material-ui/core"
 
 const IndexPage = () => (
   <Layout>
-    <Typography variant="h6">Advent of Code 2019</Typography>
+    <Typography component="h1" variant="h6">
+      Advent of Code 2019
+    </Typography>
     <Typography>
       This years Advent of Code, implemented in the browser, using typescript.
     </Typography>
