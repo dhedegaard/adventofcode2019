280dfe499a7bb812a8e8cfe551c9f2638b2e9dcb
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-10-26 10:06:15 +0200

    Added a favicon.

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
+        icon: `static/favicon.png`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
