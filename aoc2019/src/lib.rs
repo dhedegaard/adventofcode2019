82be2a030a1c0b7902c9eea6903896a5eaed65bf
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-10-27 15:24:13 +0100

    Added day18.

--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -3,6 +3,7 @@ extern crate wasm_bindgen;
 use wasm_bindgen::prelude::*;
 
 mod day01;
+mod day18;
 
 #[wasm_bindgen]
 pub fn day01_input() -> String {
@@ -15,4 +16,17 @@ pub fn day01_part1(input: &str) -> String {
 #[wasm_bindgen]
 pub fn day01_part2(input: &str) -> String {
   day01::part2(&day01::parse_input(input)).to_string()
+}
+
+#[wasm_bindgen]
+pub fn day18_input() -> String {
+  day18::get_input()
+}
+#[wasm_bindgen]
+pub fn day18_part1(input: &str) -> String {
+  day18::part1(&day18::parse_input(input)).to_string()
+}
+#[wasm_bindgen]
+pub fn day18_part2(input: &str) -> String {
+  day18::part2(&day18::parse_input(input)).to_string()
 }
