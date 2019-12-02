b51c434c409374f3d88536089bc84489d4a6581f
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-12-02 16:40:08 +0100

    Removed day18 from last year

--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -4,7 +4,6 @@ use wasm_bindgen::prelude::*;
 
 mod day01;
 mod day02;
-mod day18;
 
 #[wasm_bindgen]
 pub fn day01_input() -> String {
@@ -30,17 +29,4 @@ pub fn day02_part1(input: &str) -> String {
 #[wasm_bindgen]
 pub fn day02_part2(input: &str) -> String {
   day02::part2(&day02::parse_input(input)).to_string()
-}
-
-#[wasm_bindgen]
-pub fn day18_input() -> String {
-  day18::get_input()
-}
-#[wasm_bindgen]
-pub fn day18_part1(input: &str) -> String {
-  day18::part1(&day18::parse_input(input)).to_string()
-}
-#[wasm_bindgen]
-pub fn day18_part2(input: &str) -> String {
-  day18::part2(&day18::parse_input(input)).to_string()
 }
