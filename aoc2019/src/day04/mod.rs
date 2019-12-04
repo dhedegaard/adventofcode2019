88c41b9153a241c1872a487f5ffa13fc825532df
Dennis Hedegaard <dennis@dhedegaard.dk>
2019-12-04 17:49:15 +0100

    day04 + wasm

--- aoc2019/src/day04/mod.rs
+++ aoc2019/src/day04/mod.rs
@@ -27,6 +27,33 @@ fn is_valid_password(input: &str) -> bool {
   adjacent_are_equal
 }
 
+fn is_valid_password2(input: &str) -> bool {
+  if input.len() != 6 {
+    return false;
+  }
+  let mut doubles_hit = false;
+  let mut group_length = 1;
+  let chars = input.chars().collect::<Vec<_>>();
+  let mut last_char: char = chars[0];
+  for c in chars.iter().skip(1) {
+    if !c.is_numeric() {
+      return false;
+    }
+    if *c < last_char {
+      return false;
+    } else if *c == last_char {
+      group_length += 1;
+    } else {
+      if group_length == 2 {
+        doubles_hit = true;
+      }
+      group_length = 1;
+    }
+    last_char = *c;
+  }
+  doubles_hit || group_length == 2
+}
+
 fn parse_input(input: &str) -> (String, String) {
   let mut inputs = input.split('-');
   (
@@ -37,10 +64,20 @@ fn parse_input(input: &str) -> (String, String) {
 
 pub fn part1(input: &str) -> usize {
   let (from, to) = parse_input(input);
-  println!("f: {}, t: {}", from, to);
   let mut valid_passwords = 0;
   for val in from.parse::<i32>().unwrap()..to.parse::<i32>().unwrap() {
     if is_valid_password(&val.to_string()) {
+      valid_passwords += 1;
+    }
+  }
+  valid_passwords
+}
+
+pub fn part2(input: &str) -> usize {
+  let (from, to) = parse_input(input);
+  let mut valid_passwords = 0;
+  for val in from.parse::<i32>().unwrap()..to.parse::<i32>().unwrap() {
+    if is_valid_password2(&val.to_string()) {
       valid_passwords += 1;
     }
   }
@@ -61,5 +98,19 @@ mod tests {
   #[test]
   fn result_part1() {
     assert_eq!(part1(&raw_input()), 1864);
+  }
+
+  #[test]
+  fn test_is_valid_password2() {
+    assert_eq!(is_valid_password2("111111"), false);
+    assert_eq!(is_valid_password2("112233"), true);
+    assert_eq!(is_valid_password2("123444"), false);
+    assert_eq!(is_valid_password2("111122"), true);
+    assert_eq!(is_valid_password2("123445"), true);
+  }
+
+  #[test]
+  fn result_part2() {
+    assert_eq!(part2(&raw_input()), 1258);
   }
 }
--- aoc2019/src/lib.rs
+++ aoc2019/src/lib.rs
@@ -45,3 +45,16 @@ pub fn day03_part1(input: &str) -> String {
 pub fn day03_part2(input: &str) -> String {
   day03::part2(&day03::parse_input(input)).to_string()
 }
+
+#[wasm_bindgen]
+pub fn day04_input() -> String {
+  day04::raw_input()
+}
+#[wasm_bindgen]
+pub fn day04_part1(input: &str) -> String {
+  day04::part1(input).to_string()
+}
+#[wasm_bindgen]
+pub fn day04_part2(input: &str) -> String {
+  day04::part2(input).to_string()
+}
